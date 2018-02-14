59130cc8df94e1484b25be1ce53af16c47433d78
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-14 18:57:36 +0000

    Removed PEP8-incompatible spaces from setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from distutils.core import setup, Extension
 
 
 module1 = Extension('pyndicapi',
-                    sources = [
+                    sources=[
                         'ndicapi.cxx',
                         'ndicapi_math.cxx',
                         'ndicapi_serial.cxx',
                         'ndicapi_thread.cxx',
                         'ndicapimodule.cxx',
                     ],
-                    libraries = ['ndicapi'],
-                   )
-setup (name = 'pyndicapi',
-       version = '3.2',
-       description = 'This package allows interfacing with NDI tracking devices',
-       ext_modules = [module1])
+                    libraries=['ndicapi'],
+                    )
+setup(name='pyndicapi',
+      version='3.2',
+      description='This package allows interfacing with NDI tracking devices',
+      ext_modules=[module1]
+      )
