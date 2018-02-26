ecf03e26423ec43f82d0de9a6f53b6f93089b11e
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-26 12:48:12 +0000

    Issue #11: using PyUnicode_AsUTF8 instead of (deprecated?) PyUnicode_AsUnicode

--- ndicapimodule.cxx
+++ ndicapimodule.cxx
@@ -23,7 +23,7 @@
   #define PyString_FromString PyUnicode_FromString
   #define PyString_FromStringAndSize PyUnicode_FromStringAndSize
   #define PyString_Format PyUnicode_Format
-  #define PyString_AsString PyUnicode_AsUnicode
+  #define PyString_AsString PyUnicode_AsUTF8
   #define PyIntObject PyLongObject
   #define PY_INT_OBJECT_OB_IVAL(ob) ob->ob_digit[0]
   #define cmpfunc PyAsyncMethods*
