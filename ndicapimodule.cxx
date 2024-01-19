92444d6afef0df10c21cb4ac2e83d4ff46c54e12
Adam Rankin <adam.rankin@gmail.com>
2024-01-18 20:21:03 -0500

    Fixes #42 Enabling build for Python 3.11+ by using new PY_SET_TYPE macro

--- ndicapimodule.cxx
+++ ndicapimodule.cxx
@@ -1748,8 +1748,13 @@ ndicapiExport MOD_INIT(ndicapy)
   PyNdicapiType.ob_type = &PyType_Type;
   PyNDIBitfield_Type.ob_type = &PyType_Type;
 #else
-  Py_TYPE(&PyNdicapiType) = &PyType_Type;
-  Py_TYPE(&PyNDIBitfield_Type) = &PyType_Type;
+  #if PY_MINOR_VERSION < 11
+    Py_TYPE(&PyNdicapiType) = &PyType_Type;
+    Py_TYPE(&PyNDIBitfield_Type) = &PyType_Type;
+  #else
+    Py_SET_TYPE(&PyNdicapiType, &PyType_Type);
+    Py_SET_TYPE(&PyNDIBitfield_Type, &PyType_Type);
+  #endif
 #endif
 
   MOD_DEF(module, "ndicapy", NULL, NdicapiMethods);
