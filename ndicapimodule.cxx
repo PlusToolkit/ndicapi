615ee27d06ddbc167745411946628bc5a5e246d5
Matt Clarkson <m.clarkson@ucl.ac.uk>
2026-09-08 19:23:52 +0100

    Issue 51 - release GIL while calling ndiCommand (#52)
    * Release GIL between calls to ndiCommand.
    
    * Decrease reference count on initial and remainder variables, as per other if blocks.

--- ndicapimodule.cxx
+++ ndicapimodule.cxx
@@ -752,11 +752,20 @@ static PyObject* Py_ndiCommand(PyObject* module, PyObject* args)
       return NULL;
     }
 
-    result = ndiCommand(pol, "%s", PyString_AsString(newstring));
+    const char* cmd_cstr = PyString_AsString(newstring);
+    // Assumes caller manages threads.
+    Py_BEGIN_ALLOW_THREADS
+    result = ndiCommand(pol, "%s", cmd_cstr);
+    Py_END_ALLOW_THREADS
   }
   else
   {
+    Py_DECREF(initial);
+    Py_DECREF(remainder);
+    // Assumes caller manages threads.
+    Py_BEGIN_ALLOW_THREADS
     result = ndiCommand(pol, NULL);
+    Py_END_ALLOW_THREADS
   }
 
   if (newstring != NULL)
